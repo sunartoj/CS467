ca9b1e83ad905628aacfdb976047376b9a7e4bef
Johanes Sunarto <sunartoj@oregonstate.edu>
2017-05-04 19:18:08 -0700

    Simple Level w Enemy Spawning and UI

---
+++ TowerDefense/TowerDefense/Assets/EnemyMovement4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement4 : MonoBehaviour {
+
+	public float speed = 10f;
+	private Transform target;
+	private int wavepointIndex = 0;
+
+	void Start() {
+		target = Waypoints4.points [0];
+	}
+
+	void Update(){
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance (transform.position, target.position) <= 0.2f) {
+			GetNextWaypoint ();
+		}
+	}
+
+	void GetNextWaypoint() {
+		if(wavepointIndex >= Waypoints4.points.Length -1) {
+			Destroy (gameObject);
+			return;
+		}
+		wavepointIndex++;
+		target = Waypoints4.points [wavepointIndex];
+	}
+}
---
+++ TowerDefense/TowerDefense/Assets/Waypoints2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints2 : MonoBehaviour {
+
+	public static Transform[] points;
+
+	void Awake() { //load the childs into the points array
+		points = new Transform[transform.childCount];
+		for (int i = 0; i < points.Length; i++) {
+			points[i] = transform.GetChild (i);
+		}
+	}
+}
