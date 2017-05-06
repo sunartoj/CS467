e364b9b1f8739521c6b95457f1b45a6da8827e9b
abuenavista <abuenavi@gmail.com>
2017-05-06 09:21:43 -0700

    Just a sample 2D

---
+++ ZombieSample-Adrian/Assets/Scripts/MoveMe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate(new Vector3(-0.15f,0,0) * Time.deltaTime);
+	}
+}
---
+++ ZombieSample-Adrian/Assets/Scripts/enemyDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour {
+
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag=="Zombie") {
+			enemyHealth eh = other.gameObject.GetComponent<enemyHealth> ();
+			eh.TakeDamage (damage);
+
+			Destroy (gameObject);
+		}
+	}
+}
