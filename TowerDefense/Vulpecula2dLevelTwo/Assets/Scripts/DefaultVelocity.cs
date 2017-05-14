0b52401df6207b50e421408a1c00e73925b17699
Johanes Sunarto <sunartoj@oregonstate.edu>
2017-05-14 02:06:15 -0700

    Added Chris Stuff

---
+++ TowerDefense/Vulpecula2dLevelTwo/Assets/Scripts/DefaultVelocity.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultVelocity : MonoBehaviour {
+    // The Velocity (can be set from Inspector)
+    public Vector2 velocity;
+
+    void FixedUpdate () {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
