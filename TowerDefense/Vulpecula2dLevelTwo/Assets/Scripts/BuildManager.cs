7b61790a8ca43c30f21cfb35a60292bce8e60191
Johanes Sunarto <sunartoj@oregonstate.edu>
2017-05-21 16:52:59 -0700

    tower placement

---
+++ TowerDefense/Vulpecula2dLevelTwo/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	// Singleton pattern, to allow build manager be accessed without a reference
+	public static BuildManager instance;
+
+	void Awake() {
+		if (instance != null) {
+			Debug.LogError ("More than one build manager exists");
+			return;
+		}
+		instance = this;
+	}
+
+	public GameObject standardTurretPrefab;
+
+	void Start() {
+		turretToBuild = standardTurretPrefab;
+	}
+
+	private GameObject turretToBuild;
+
+	public GameObject GetTurretToBuild (){
+		return turretToBuild;
+	}
+}
