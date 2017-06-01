513ab2276b7c8838cd57968c9319ff1bb4d8cd43
abuenavista <abuenavi@gmail.com>
2017-06-01 01:12:38 -0700

    some updates on zombie sample

--- ZombieSample-Adrian/Assets/Scripts/TowerBtn.cs
+++ ZombieSample-Adrian/Assets/Scripts/TowerBtn.cs
@@ -1,11 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerBtn : MonoBehaviour {
 
     [SerializeField]
     private GameObject towerPrefab;
+
+    GameManager gm;
+    Button thisButton;
+
+    private void Start()
+    {
+        gm = GameManager.instance;
+
+        if (gameObject.name == "CowgirlBtn")
+        {
+            if (gm.level < 2 || gm.currGold < towerPrefab.GetComponent<ninjaCtrl>().cost)
+            {
+                //thisButton = GameObject.Find("CowgirlBtn").GetComponent<Button>();
+                this.GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                this.GetComponent<Button>().interactable = true;
+            }
+
+        }
+
+        if (gameObject.name == "RobotBtn")
+        {
+            if (gm.level < 3 || gm.currGold < towerPrefab.GetComponent<ninjaCtrl>().cost)
+            {
+                //thisButton = GameObject.Find("CowgirlBtn").GetComponent<Button>();
+                this.GetComponent<Button>().interactable = false;
+            }
+            else
+            {
+                this.GetComponent<Button>().interactable = true;
+            }
+
+        }
+    }
 
     public GameObject TowerPrefab
     {
